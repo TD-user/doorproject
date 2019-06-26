34b745dd0a3763ed5d047db2df8bdd3ea6e8f8d3
KSerjyk <serg14581@gmail.com>
2019-06-26 16:46:19 +0300

    adding Params to Lock

--- EntitiesDB/Lock.cs
+++ EntitiesDB/Lock.cs
@@ -5,6 +5,28 @@
 
         public int ID { get; set; }
         public string Name { get; set; }
+        public short X { get; set; }
+        public short X1 { get; set; }
+        public short X2 { get; set; }
+        public short X3 { get; set; }
+        public short X4 { get; set; }
+        public short X5 { get; set; }
+        public short X6 { get; set; }
+        public short Y { get; set; }
+        public short Y1 { get; set; }
+        public short Y2 { get; set; }
+        public short Y3 { get; set; }
+        public short Z1 { get; set; }
+        public short Z2 { get; set; }
+        public short Z3 { get; set; }
+        public short Z4 { get; set; }
+        public short d1 { get; set; }
+        public short R1 { get; set; }
+        public short R2 { get; set; }
+        public bool EnableOP1 { get; set; }
+        public bool EnableOP2 { get; set; }
+        public bool EnableOP3 { get; set; }
+        public bool EnableOP4 { get; set; }
         public Lock(){}
 
         public Lock(int iD, string name)
